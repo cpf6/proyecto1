49a1a788de7785506a1f37ab6c8c2ac11198c35c
cpf6 <carolinafantu2014@gmail.com>
2025-05-29 11:56:39 -0300

    Correcciones

--- escenas/componente/enemigo/enemigo_volador.gd
+++ escenas/componente/enemigo/enemigo_volador.gd
@@ -1,21 +1,28 @@
 extends Enemigos
- 
-@export var hitbox : Area2D
 
-@onready var posicion_Inicial : Vector2 = position 
-var distancia : float = 100
-var velocidad : float = 5000
-var direccion : Vector2 = Vector2(1,0)
+@export var hitbox: Area2D
+@export var amplitud: float = 60
+@export var periodo: float = 2.0
+@export var velocidad_x: float = 300
+@export var distancia_x: float = 400
 
-func _physics_process(delta : float) -> void: 
-	velocity = direccion * velocidad * delta
-	if position.distance_to(posicion_Inicial) >= distancia: 
-		direccion = position.direction_to(posicion_Inicial)
-	move_and_slide()
+@onready var posicion_inicial : Vector2 = position
 
+var tiempo: float = 0.0
+var dir_x: int = 1
 
-func _on_timer_ataque_timeout():
+func _physics_process(delta : float) -> void:
+	position.x += dir_x * velocidad_x * delta
+
+	tiempo += delta
+	var offset_y := sin(tiempo * TAU / periodo) * amplitud
+	position.y = posicion_inicial.y + offset_y
+
+	if abs(position.x - posicion_inicial.x) >= distancia_x:
+		dir_x *= -1
+		tiempo = 0.0
+
+func _on_timer_timeout():
 	for cuerpo in hitbox.get_overlapping_bodies():
 		if cuerpo.is_in_group("Jugador"):
-			cuerpo.herir(25) 
-			print ("jugadorherido")
+			cuerpo.herir(50)
