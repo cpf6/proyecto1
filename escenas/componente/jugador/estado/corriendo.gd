9d1df228c11bc87a3e5823d844845730fa5f807c
cpf6 <carolinafantu2014@gmail.com>
2025-05-28 23:29:45 -0300

    Tilemap

--- escenas/componente/jugador/estado/corriendo.gd
+++ escenas/componente/jugador/estado/corriendo.gd
@@ -7,14 +7,21 @@ func physics_update(delta):
 	var direccion = Input.get_axis("mover_izquierda","mover_derecha")
 	
 	jugador.velocity.x = direccion * jugador.velocidad * delta
+	if not jugador.is_on_floor():
+		jugador.velocity.y += jugador.gravedad * delta
 	jugador.move_and_slide()
+
 		
 	if jugador.velocity.x > 0:
 		jugador.sprite.flip_h = false
 	elif jugador.velocity.x < 0:
 		jugador.sprite.flip_h = true
 	
-	if jugador.velocity.x == 0:
+	if direccion == 0:
 		get_parent().ir_a_estado_siguiente("Quieto")
+
+	if Input.is_action_just_pressed("saltar") and jugador.is_on_floor():
+		get_parent().ir_a_estado_siguiente("Saltar")
+
 	if Input.is_action_just_pressed("atacar"):
 		get_parent().ir_a_estado_siguiente("Atacar")
--- escenas/componente/jugador/jugador.gd
+++ escenas/componente/jugador/jugador.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var velocidad : float = 10000.0
 @export var impulso_salto = 1200.0
 @export var friccion_salto = 100.0
-@export var gravedad = 7500.0
+@export var gravedad = 6000.0
 @export var sprite : AnimatedSprite2D
 
 @export var hud : Control
